570f732af65881c9432cf3b0f59ef06864c713ce
kirtan1432 <133342879+kirtan1432@users.noreply.github.com>
2023-07-12 13:33:31 +0530

    even_2d(4).c

--- even_forth/even_2d(4).c
+++ even_forth/even_2d(4).c
@@ -13,18 +13,20 @@ void main ()
 	
 	int a[n][m];
 	int i,j;
+	int *ptr[100][100];
+	ptr[100][100]=&a[n][m];
 	
 	for(i=0;i<n;i++){
 		for(j=0;j<m;j++){
 			printf("a[%d][%d] => ",i,j);
-			scanf("%d",&a[i][j]);
+			scanf("%d",&ptr[i][j]);
 		}
 	}
 	printf("\n");
 	for(i=0;i<n;i++){
 		for(j=0;j<m;j++){
-			if(a[i][j]%2==0){
-				printf("%d\t",a[i][j]);
+			if(*ptr[i][j]%2==0){
+				printf("%d\t",ptr[i][j]);
 				
 			}
 		}
